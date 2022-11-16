b03bd72c6f9bf4235d5e1546fcdcbdfa2de84a9d
L4UTY161603 <lautykaufmann76@gmail.com>
2022-11-16 20:57:59 -0300

    Se agregan nuevos cambios al menu

--- src/com/tetris/Main.java
+++ src/com/tetris/Main.java
@@ -9,13 +9,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                JFrame frame = new InicioForm(300,300);
-                 frame.setVisible(true);
-                 frame.setLocationRelativeTo(null);
-            }
-        });
+        InicioForm form = new InicioForm();
+        form.setVisible(true);
+        form.setLocationRelativeTo(null);
     }
 }
